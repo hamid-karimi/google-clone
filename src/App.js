899f3e4b18b33cc5f794475c5f4b5daa7623fbc1
Hamid <sir.lovelorne@gmail.com>
2021-10-27 17:41:13 +0330

    initialize
    first initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import {Navbar} from './components/Navbar'
+import {Footer} from './components/Footer'
+import {Routes} from './components/Routes'
+ const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false)
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className = "bg-gray-100">
+        <Navbar />
+        <Footer />
+        <Routes />
+      </div>
     </div>
   );
 }
